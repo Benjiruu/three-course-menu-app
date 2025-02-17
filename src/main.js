a3c9681f7d13016cdce06f6fbe2a0d6d05399105
Benjiruu <benny_91@hotmail.com>
2025-02-17 17:04:39 +0100

    dsadsada

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'https://unpkg.com/vue@next';
-import { createRouter, createWebHistory } from 'https://unpkg.com/vue-router@next';
+import { createApp } from 'https://unpkg.com/vue@3.2.31/dist/vue.esm-browser.js';
+import { createRouter, createWebHistory } from 'https://unpkg.com/vue-router@4.0.12/dist/vue-router.esm-browser.js';
 import App from './App.vue';
 import routes from './router';
 
